f2d292ad5dbe64981ad5d5d8be449b8ee00b9791
eleaacrt <eleaa.crt@gmail.com>
2022-10-19 16:22:36 +0200

    debut pourcentages

--- js/datareasons.js
+++ js/datareasons.js
@@ -47,6 +47,11 @@ fetch('./json/data_reasons.json').then(function(res) {
                 square.classList.add("" + reason + "");
                 wrapperBlock.appendChild(square).innerHTML += '<svg id="blanksvg' + reason + '" width="50" height="50" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M40 55.3199C44.577 55.3199 48.8126 53.8583 52.2656 51.3765C54.2195 49.9721 57.0585 50.0213 58.2664 52.1023C61.4644 57.6118 63.3766 64.5997 63.3766 72.203C56.8831 76.2729 52.9106 80 40 80C27.0894 80 21.7336 75.5951 16.6234 72.203C16.6234 64.5997 18.5356 57.6118 21.7336 52.1023C22.9415 50.0213 25.7805 49.9721 27.7344 51.3765C31.1874 53.8583 35.423 55.3199 40 55.3199Z" fill="#F1F7ED"/><path d="M58.1818 33.7662C58.1818 43.8078 50.0415 51.9481 40 51.9481C29.9585 51.9481 21.8182 43.8078 21.8182 33.7662C21.8182 23.7247 29.9585 15.5844 40 15.5844C50.0415 15.5844 58.1818 23.7247 58.1818 33.7662Z" fill="#F1F7ED"/></svg>';
             })
+
+            data.forEach(function viewData(reason) {
+                document.querySelector(".percentage").innerHTML +=
+                    '<p class="' + reason.reason + 'percentage">' + reason.percentage + '% </p>';
+            })
         }
     });
 });
@@ -100,6 +105,11 @@ function evidenceRemoveOther() {
     $(this).css("cursor", null).css("color", "#f1f7ed");;
 };
 
+
+
+function showPercentage() {
+
+};
 
 
 $business.on('mouseover', businessHover).on('mouseleave', evidenceRemoveBusiness);
