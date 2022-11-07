2c38d06ba170e99c39ca4d3b6fa65eb8f1dbf4c4
Amel Chabah <amelou518@gmail.com>
2022-11-07 22:12:26 +0100

    datamap et changements

--- js/script.js
+++ js/script.js
@@ -1,24 +1,5 @@
 // récupération des données en json
 
-// fetch("./json/data_reasons.json")
-//     .then(res => res.json())
-//     .then(data => console.log(JSON.stringify(data)));
-
-document.addEventListener("DOMContentLoaded", function() {
-    //lier le data.json
-    // fetch('./json/data_reasons.json').then(function(res) {
-    //     res.json().then(function(data) {
-    //         data.forEach(function viewData(reason) {
-    //             document.querySelector('.data-reasons').innerHTML += '<p class=' + reason.reason + '>' + reason.reason + ' : ' + reason.percentage + '</p>'
-    //         })
-    //     })
-    // });
-
-});
-
-
-
-
 // animations
 AOS.init();
 
