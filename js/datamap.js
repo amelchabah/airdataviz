f94de28c5bf35018f39ae530322030831f9bb544
Amel Chabah <amelou518@gmail.com>
2022-11-07 20:29:07 +0100

    retouches amel

--- js/datamap.js
+++ js/datamap.js
@@ -1,56 +1,37 @@
-
 // data - map
+
+let $after = $('#after');
+$after.on('click', switchRed);
+
+let $before = $('#before');
+$before.on('click', switchGreen);
+
+
+
+// continents hover
 
 let $continent = $('.continent');
 $continent.on('mouseenter', opacityOnHover).on('mouseleave', opacityRemove);
 
 let $America = $('#america'),
     $AmericaTooltip = $('#america-tooltip');
-
 $America.on('mousemove', showTooltip).on('mouseleave', hideTooltip);
 
 let $europe = $('#europe'),
     $europeTooltip = $('#europe-tooltip');
-
 $europe.on('mousemove', showTooltip).on('mouseleave', hideTooltip);
-
-
-// let $westAfrica = $('#west-africa'),
-//     $westAfricaTooltip = $('#west-africa-tooltip');
-
-// $westAfrica.on('mousemove', showTooltip).on('mouseleave', hideTooltip);
 
 let $northAfrica = $('#north-africa'),
     $northAfricaTooltip = $('#north-africa-tooltip');
-
 $northAfrica.on('mousemove', showTooltip).on('mouseleave', hideTooltip);
-
-// let $southAfrica = $('#south-africa'),
-//     $southAfricaTooltip = $('#south-africa-tooltip');
-
-// $southAfrica.on('mousemove', showTooltip).on('mouseleave', hideTooltip);
 
 let $Africa = $('#africa'),
     $AfricaTooltip = $('#africa-tooltip');
-
 $Africa.on('mousemove', showTooltip).on('mouseleave', hideTooltip);
-
-
-// var $russia = $('#russia'),
-//     $russiaTooltip = $('#russia-tooltip');
-
-// $russia.on('mousemove', showTooltip).on('mouseleave', hideTooltip);
 
 var $southAsia = $('#south-asia'),
     $southAsiaTooltip = $('#south-asia-tooltip');
-
 $southAsia.on('mousemove', showTooltip).on('mouseleave', hideTooltip);
-
-var $australia = $('#australia'),
-    $australiaTooltip = $('#australia-tooltip');
-
-$australia.on('mousemove', showTooltip).on('mouseleave', hideTooltip);
-
 
 function showTooltip(event) {
     var el = event.currentTarget.id,
@@ -85,4 +66,24 @@ function opacityRemove() {
     $(".continent").css("opacity", "1");
 };
 
+function switchRed() {
+    $('#after').attr("class","white").css('transition','0.5s').css('cursor',"default");
+    $('#before').attr("class","red").css('transition','0.5s').css('cursor',"pointer");
+    $('.data-map article').attr("class", "red").css('transition','0.5s');
+    $('#america g').attr("class", "red").css('transition','0.5s');
+    $('#africa g').attr("class", "red").css('transition','0.5s');
+    $('#europe g').attr("class", "red").css('transition','0.5s');
+    $('#south-asia g').attr("class", "red").css('transition','0.5s');
+    $('#north-africa g').attr("class", "red").css('transition','0.5s');
+}
 
+function switchGreen() {
+    $('#after').attr("class", "green").css('transition','0.5s').css('cursor',"pointer");
+    $('#before').attr("class","white").css('transition','0.5s').css('cursor',"default");
+    $('.data-map article').attr("class", "green").css('transition','0.5s');
+    $('#america g').attr("class", "green").css('transition','0.5s');
+    $('#africa g').attr("class", "green").css('transition','0.5s');
+    $('#europe g').attr("class", "green").css('transition','0.5s');
+    $('#south-asia g').attr("class", "green").css('transition','0.5s');
+    $('#north-africa g').attr("class", "green").css('transition','0.5s');
+}
